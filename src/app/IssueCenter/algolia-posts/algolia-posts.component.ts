fcce2fd9bfe9b99c450d1c6e089348ee6b03b944
minoshiLiyanage <chandimal.nipuna.1995@gmail.com>
2020-02-19 19:14:06 +0530

    removed notification component

--- src/app/IssueCenter/algolia-posts/algolia-posts.component.ts
+++ src/app/IssueCenter/algolia-posts/algolia-posts.component.ts
@@ -19,31 +19,24 @@ export class AlgoliaPostsComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
   ngOnInit() {
     
-    if(this.route.snapshot.paramMap.get('id')!=null){ 
-      //get database name using URL
+    if(this.route.snapshot.paramMap.get('id')!=null){
+      console.log("ID is not null");
+      console.log(parseInt(this.route.snapshot.paramMap.get('id')));
+      console.log(selector.select(parseInt(this.route.snapshot.paramMap.get('id'))).toLowerCase());
+      
       this.id = parseInt(this.route.snapshot.paramMap.get('id'));
-      this.postCatagory = selector.test(this.id);
+      
+      this.postCatagory = selector.select(this.id);
       this.postCatagoryLowecase = this.postCatagory.toLowerCase();
       this.databaseName = this.postCatagory.replace(" ","_").replace(" ","_");
-      this.routerLink = "../../newPost/" + this.id;
+      this.routerLink = "../../newPost/"+this.id;
  
     }else{
       this.postCatagory = "error";
     }
   } 
-
-   //there will be no search results as default
-   showResults = true ;
-
-   //change search results showing status 
-   searchChanged(query){
-     if(query.length){
-       this.showResults = true;
-     }else{
-       this.showResults = false;
-     }
-
-    }
+   
+  
 
   
    
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,5 @@
 import { Component } from "@angular/core";
-import {
-  AngularFirestoreCollection,
-  AngularFirestore
-} from "angularfire2/firestore";
-import { Observable } from "rxjs";
-import { skip } from "rxjs/operators";
-import { Notification } from "../common/Notification.interface";
-import { NotificationsService } from "angular2-notifications";
-import { ActivatedRoute } from "@angular/router";
-import { AuthService } from "../login/services/auth.service";
+ 
 
 @Component({
   selector: "app-navbar",
@@ -16,63 +7,8 @@ import { AuthService } from "../login/services/auth.service";
   styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent {
-  //create the bell icon ring + notification bubble
-  public newNotifications: boolean;
-  private notificationCollection: AngularFirestoreCollection<Notification>;
-  notification: Observable<Notification[]>;
-  private usersUID: string;//stores user's UID
-  notificationsCollection: AngularFirestoreCollection<Notification>;
-
-  constructor(public afs: AngularFirestore,private notify: NotificationsService,private router: ActivatedRoute,private auth: AuthService) {
-   
-    //if user logged in get his uid from localStorage
-    if(localStorage.getItem('user') !="null" ){
-      const user = JSON.parse(localStorage.getItem('user'));
-      this.usersUID = user['uid'].replace('"', "").replace('"', "")   
-    }
-   
-    
-    this.newNotifications = false;
-    this.notificationCollection = afs.collection<Notification>(`Notifications/${this.usersUID}/${this.usersUID}/`); //5xmXcPPVmPgWi4uttnN2v6rrMmD2/fEsNdG9LaHNPK2f3LYuH/
-    this.notification = this.notificationCollection.valueChanges();
-    
-    //don't display notifications in the detailed notification page
-    if (this.router.pathFromRoot.toString() =="Route(url:'', path:''),Route(url:'not', path:'not')"){
-      console.log("You are in notification page ");
-    }else{ 
-      console.log("You are not in notification page ");
-      this.notification.pipe(skip(1)).subscribe(
-        //skip the inital notification message
-        notification => {  
-          this.newNotifications = true;  
-          this.createNotification( );
-        },
-        err => console.log(err),
-        () => console.log("stream is finished")
-      );
-    }
-  }
-
-  update(notification: Notification) {
-    this.notificationsCollection.add(notification);
-  }
-
-  //properties of bottom notification
-  createNotification() { 
-    this.notify.success("New answer added !", " Check  notifications to see details  ", {
-      timeOut: 3000,
-      showProgressBar: true,
-      pauseOnHover: true,
-      clickToClose: false,
-      clickIconToClose: true,
-      maxStack:1
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.notification !== null) {
-      this.notification == null;
-      console.log("Obseravable object destroyed !");
-    }
+ 
+  constructor () {
+  
   }
 }
